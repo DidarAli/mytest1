a7108754a2e7c519b0a48809df246659180f133b
Ali <didar@mail.ru>
2017-09-26 02:23:22 +0600

    experiment1 бұтағында Hello.java және MyMath.java файлдарына өзгерістер жасалды.

--- Src/kz/kaznitu/lessons/Hello.java
+++ Src/kz/kaznitu/lessons/Hello.java
@@ -4,11 +4,12 @@ public class Hello {
     public static void main(String[] args){
         System.out.println("Hello World!");
         System.out.println("Бұл зертханалық жұмыстың мақсаты:");
-        System.out.println("GitHub-пен жұмыс жасап үйрену.");
+        System.out.println("GitHub-пен жұмыс жасап үйренуу.");
         int a=MyMath.kvadrat(5);
         int b=MyMath.kub(5);
         System.out.println("a = "+a);
         System.out.println("b = "+b);
+
         System.out.println(MyMath.max(8,9));
 
     }
