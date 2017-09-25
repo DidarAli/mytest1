e86b2c95ef12d432f8fa7827bbb073797198e4b6
Ali <didar@mail.ru>
2017-09-26 01:10:27 +0600

    qwerty

--- Src/kz/kaznitu/lessons/Hello.java
+++ Src/kz/kaznitu/lessons/Hello.java
@@ -3,5 +3,8 @@ package kz.kaznitu.lessons;
 public class Hello {
     public static void main(String[] args){
         System.out.println("Hello World!");
+        System.out.println("Бұл зертханалық жұмыстың мақсаты:");
+        System.out.println("GitHub-пен жұмыс жасап үйрену.");
+
     }
 }
